9122406500f726c39fc9b2143a5c7df95e9cdf61
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-07-20 16:19:19 +0100

    Working emission of file lists.  On branch master  Your branch is ahead of 'origin/master' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   ../notes/gui-mockup/list.html
    	modified:   ../notes/gui-mockup/script.js
    	modified:   ../notes/gui-mockup/style.css

--- notes/gui-mockup/script.js
+++ notes/gui-mockup/script.js
@@ -3,6 +3,10 @@
 Contains all of the functions for the documentation pages.
 */
 
+/*
+Responsible for taking a JSON data structure and turning it into a list
+on the list.html page.
+*/
 function veridoc_render_list(
     data
 ){
@@ -16,4 +20,40 @@ function veridoc_render_list(
 
     // This is a <ul> element
     var container = document.getElementById('list-container');
+    container.innerHTML = "";
+
+    var toset = ""
+
+    listData = listData.sort(function(a,b){
+        if(a.path>=b.path){
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    });
+
+    var i;
+    for(i = 0; i < listData.length; i++)
+    {
+        var item = listData[i];
+        
+        toset += "<li>";
+        if(item.parsed == "1"){
+            toset += "<div class='status good'>Found</div>";
+        } else {
+            toset += "<div class='status bad'>Not Found</div>";
+        }
+        if(item.success== "1"){
+            toset += "<div class='status good'>Parsed</div>";
+        } else {
+            toset += "<div class='status bad'>Errors</div>";
+        }
+        
+        toset += "<div class='item'>"+item.path+"</div>";
+        toset += "</li>"
+
+    }
+
+    container.innerHTML = toset;
 }
