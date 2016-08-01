3d3a51c172d13a9d3fc8c589457846cf32e10ffb
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-08-01 13:50:19 +0100

    Split separate coverage and debug builds.  On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   bin/project.sh
    	modified:   src/CMakeLists.txt

--- bin/project.sh
+++ bin/project.sh
@@ -4,11 +4,19 @@ echo "------------------- Setting Up Verilog-Doc Workspace -----------------"
 
 git submodule update --remote src/verilog-parser/
 
+rm -rf ./build
+
 mkdir -p ./build/
+mkdir -p ./build/debug
+mkdir -p ./build/coverage
 mkdir -p ./build/docs
 
-cd ./build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cd ./build/debug
+cmake -DCMAKE_BUILD_TYPE=Debug ../../
+cd -
+
+cd ./build/coverage
+cmake -DCMAKE_BUILD_TYPE=Debug -DVERIDOC_COVERAGE=YES ../../
 cd -
 
 echo "----------------------- Workspace setup complete ---------------------"
